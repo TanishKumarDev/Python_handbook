f8556d83347283fec123123e14ac5ac55378aa05
Tanish <tanish.kumar.contact@gmail.com>
2025-08-28 06:10:33 +0530

    Add Python basics: conditional statements, data types, input/output, operators, and variables

---
+++ Keywords.py
@@ -0,0 +1,7 @@
+# Getting List of all Python keywords
+import keyword
+
+print("Python Keywords:")
+print(keyword.kwlist) # # printing all keywords at once using "kwlist()"
+
+# What Happens if We Use Keywords as Variable Names ? -> # SyntaxError: invalid syntax
